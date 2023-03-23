391941dfc0ff0b2d889296966d213ca052f0e2a5
POPA Andrei-Valentin <Andrei-Valentin.POPA@cegedim.com>
2023-03-23 18:50:26 +0200

    Display cards for products in overview page from json

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const fs = require("fs");
 const http = require ("http");
 
+const replaceTemplate = (template, product) => {
+ let output = template.replace(/{%PRODUCT_NAME%}/g, product.productName);
+ output = output.replace(/{%IMAGE%}/g, product.image)
+ output = output.replace(/{%PRICE%}/g, product.price)
+ output = output.replace(/{%PRODUCT_NUTRIENTS%}/g, product.nutrients)
+ output = output.replace(/{%QUANTITY%}/g, product.quantity)
+ output = output.replace(/{%DESCRIPTION%}/g, product.description)
+ output = output.replace(/{%ID%}/g, product.id)
+
+ if(!product.organic) {
+  output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic")
+ }
+
+ return output;
+}
+
 const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
 const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
@@ -13,8 +29,12 @@ const server = http.createServer((req, res) =>{
 
  // Overview Page
  if (pathName === "/" || pathName === "overview") {
-  res.writeHead(200, {'Content-type':'application/json'});
-  res.end(templateOverview);
+  res.writeHead(200, {'Content-type':'text/html'});
+
+  const cardsHtml = dataObj.map(el => replaceTemplate(templateCard, el)).join('')
+  const output = templateOverview.replace("{%PRODUCT_CARDS%}",cardsHtml)
+
+  res.end(output);
 
   // Product Page
  } else if (pathName === "/product") {
