7faafa35b861f1f7e999f936f1c3e3f215892eed
POPA Andrei-Valentin <Andrei-Valentin.POPA@cegedim.com>
2023-03-23 17:44:04 +0200

    added serverListen and preparing for template implementation

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const fs = require("fs");
 const http = require ("http");
 
+const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) =>{
  const pathName = req.url;
 
+ // Overview Page
  if (pathName === "/" || pathName === "overview") {
-  res.end("This is the overview");
+  res.writeHead(200, {'Content-type':'application/json'});
+  res.end(templateOverview);
+
+  // Product Page
  } else if (pathName === "/product") {
   res.end("This is the product")
+
+  // API 
  } else if (pathName === "/api") {
   res.writeHead(200, {'Content-type':'application/json'});
   res.end(data);
+
+  // NOT FOUND
  } else {
   res.writeHead(404,{
    "Content-type": "text/html",
@@ -21,3 +33,7 @@ const server = http.createServer((req, res) =>{
   })
  }
 })
+
+server.listen(8000, '127.0.0.1', () => {
+ console.log('Listening to requests on port 8000');
+});
